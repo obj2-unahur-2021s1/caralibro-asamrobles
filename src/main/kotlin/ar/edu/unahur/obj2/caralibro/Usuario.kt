b713f5f380644aea79c9ae00837168d276e8a555
Luciano Robles <soylucho@hotmail.es>
2021-04-17 21:21:54 -0300

    Amigos y algo mas
    hicimos punto 3 de los requerimientos

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
@@ -2,11 +2,23 @@ package ar.edu.unahur.obj2.caralibro
 
 class Usuario {
   val publicaciones = mutableListOf<Publicacion>()
+  val amigos = mutableListOf<Amigo>()
 
   fun agregarPublicacion(publicacion: Publicacion) {
     publicaciones.add(publicacion)
   }
+  fun agregarAmigo(amigoNuevo :Amigo) {amigos.add(amigoNuevo)}
+
+  fun cantidadDeAmigos() = amigos.size
 
   fun espacioDePublicaciones() = publicaciones.sumBy { it.espacioQueOcupa() }
 
+  fun cualEsMasAmistoso(usuario1: Usuario ,usuario2 : Usuario) : Usuario {
+    var elMasAmistoso = Usuario()
+    if (usuario1.cantidadDeAmigos() > usuario2.cantidadDeAmigos()) {
+      elMasAmistoso = usuario1
+    }
+      else elMasAmistoso = usuario2
+    return elMasAmistoso
+  }
 }
--- src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
@@ -6,12 +6,29 @@ import io.kotest.matchers.shouldBe
 
 class UsuarioTest : DescribeSpec({
   describe("Caralibro") {
+    // Publicaciones
     val saludoCumpleanios = Texto("Felicidades Pepito, que los cumplas muy feliz")
     val fotoEnCuzco = Foto(768, 1024)
     val videoDeCumpleanios = Video ("SD", 3000)
     val videoDeCumpleaniosEn720 = Video ("HD720", 3000)
     val videoDeCumpleniosEn1080 = Video ("HD1080", 3000)
+    // Amigos
+    val ferAsam = Amigo()
+    val luchoRobles = Amigo()
+    val faloiFede = Amigo()
+    // Usuarios
+    val juana = Usuario()
+    val pepe = Usuario()
+    
+    // Acciones Sobre los usuarios
+    juana.agregarPublicacion(fotoEnCuzco)
+    juana.agregarPublicacion(saludoCumpleanios)
+    juana.agregarPublicacion(videoDeCumpleanios)
+    juana.agregarAmigo(ferAsam)
+    juana.agregarAmigo(luchoRobles)
+    juana.agregarAmigo(faloiFede)
 
+    // Acciones Sobre las publicaciones
     fotoEnCuzco.darMeGusta()
     fotoEnCuzco.darMeGusta()
     fotoEnCuzco.darMeGusta()
@@ -56,14 +73,27 @@ class UsuarioTest : DescribeSpec({
     }
 
     describe("Un usuario") {
-      val juana = Usuario()
-      juana.agregarPublicacion(fotoEnCuzco)
-      juana.agregarPublicacion(saludoCumpleanios)
-      juana.agregarPublicacion(videoDeCumpleanios)
 
       it("puede calcular el espacio que ocupan sus publicaciones") {
-
         juana.espacioDePublicaciones().shouldBe(553548)
+      }
+
+      it (" Cuantos amigos tiene juana"){
+        juana.cantidadDeAmigos().shouldBe(3)
+      }
+      it (" Cuantos amigos tiene pepe"){
+        pepe.cantidadDeAmigos().shouldBe(0)
+      }
+      it (" Cual es mas amistoso pepe o juana : Es Juana") {
+        juana.cualEsMasAmistoso(juana, pepe).shouldBe(juana)
+      }
+      pepe.agregarAmigo(luchoRobles)
+      pepe.agregarAmigo(luchoRobles)
+      pepe.agregarAmigo(luchoRobles)
+      pepe.agregarAmigo(luchoRobles)
+
+      it ("Cual es mas amistoso pepe o juana : Es Pepe") {
+        juana.cualEsMasAmistoso(juana, pepe).shouldBe(pepe)
       }
     }
   }
